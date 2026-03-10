74f24228b2c644a08c4540aa22c4e828bfd31866
Bad Lab <vincentmarinot@gmail.com>
2026-03-10 10:38:22 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const form = document.getElementById("add-form")
 const queueList = document.getElementById("queue-list")
+const cordeurList = document.getElementById("cordeur-list")
+const cordeurBtn = document.getElementById("cordeur-btn")
+const cordeurPanel = document.getElementById("cordeur-panel")
 
 let queue = JSON.parse(localStorage.getItem("queue")) || []
 
 function saveQueue(){
-localStorage.setItem("queue", JSON.stringify(queue))
+
+localStorage.setItem("queue",JSON.stringify(queue))
+
 }
 
 function renderQueue(){
@@ -13,12 +18,12 @@ queueList.innerHTML=""
 
 queue.forEach((item,index)=>{
 
-let li=document.createElement("li")
-
 let attente=(index+1)*30
 
+let li=document.createElement("li")
+
 li.innerHTML=
-"<strong>#"+(index+1)+"</strong> - "+
+"#"+(index+1)+" - "+
 item.player+
 " | "+
 item.racket+
@@ -26,10 +31,54 @@ item.racket+
 item.string+
 " | "+
 item.tension+
-" kg"+
+"kg"+
 " | ⏱ "+attente+" min"
 
 queueList.appendChild(li)
+
+})
+
+}
+
+function renderCordeur(){
+
+cordeurList.innerHTML=""
+
+queue.forEach((item,index)=>{
+
+let li=document.createElement("li")
+
+let btn=document.createElement("button")
+
+btn.innerText="Raquette terminée"
+
+btn.className="finish-btn"
+
+btn.onclick=function(){
+
+queue.splice(index,1)
+
+saveQueue()
+
+renderQueue()
+
+renderCordeur()
+
+}
+
+li.innerHTML=
+"#"+(index+1)+" - "+
+item.player+
+" | "+
+item.racket+
+" | "+
+item.string+
+" | "+
+item.tension+"kg "
+
+li.appendChild(btn)
+
+cordeurList.appendChild(li)
 
 })
 
@@ -45,17 +94,38 @@ let string=document.getElementById("string-type").value
 let tension=document.getElementById("tension").value
 
 queue.push({
+
 player,
 racket,
 string,
 tension
+
 })
 
 saveQueue()
+
 renderQueue()
+
+renderCordeur()
 
 form.reset()
 
 })
 
+cordeurBtn.onclick=function(){
+
+if(cordeurPanel.style.display==="none"){
+
+cordeurPanel.style.display="block"
+
+}else{
+
+cordeurPanel.style.display="none"
+
+}
+
+}
+
 renderQueue()
+
+renderCordeur()
